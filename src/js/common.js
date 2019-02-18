4dc915a50067a78f9137a77dd148a568eec2f8a2
Keaun Moughari <mougharik@gmail.com>
2019-02-17 20:37:17 -0500

    Update common scripts to include event listeners for mobile menu

--- src/js/common.js
+++ src/js/common.js
@@ -1,29 +1,70 @@
+/*
+ *             // common.js //
+ *  Common functionality for the FSU ACM static website
+ * 
+ * Author:  Keaun Moughari <moughari@cs.fsu.edu>
+ * Date:    Thursday, February 14th, 2019
+ * 
+ */
+
+
 addCurrent();
 calcHeight();
+heroClickEvent();
+mobMenuClickEvent();
+
 window.addEventListener('scroll', () => {
     let fsu = document.querySelector('div.fsu') || document.querySelector('div.fsu-hide');
-    if (window.scrollY > 150)
+    if (window.scrollY > 100)
         fsu.className = 'fsu-hide';
     else
         fsu.className = 'fsu';
 });
 
-document.querySelector('div.to-content').addEventListener('click', () => {
-    document.querySelector('div.content').scrollIntoView({
-        behavior: 'smooth'
-    });
-});
+
+function mobMenuClickEvent() {
+    let innerMenu = document.querySelector('div#mobile'),
+        menu = document.querySelector('div.menu');
+    document.querySelector('div.menu-icon').addEventListener('click', () => {
+            if (!innerMenu.className.includes('active')) {
+                innerMenu.className += '-active';
+                menu.className += '-show';
+            }
+            else {
+                innerMenu.className = 'inner';
+                menu.className = 'menu';
+            }
+        }
+    );
+};
+
+function heroClickEvent() {
+    if (window.location.href.includes('index.html')) {
+        document.querySelector('div.to-content').addEventListener('click', () => {
+            document.querySelector('div.content').scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    }
+};
 
 function calcHeight() {
-    document.querySelector('div.hero').style.height = String(window.innerHeight - 80) + 'px';
-    document.querySelector('div.content').style.height = String(window.innerHeight - 80) + 'px';
+    let navHeight = document.querySelector('header').getBoundingClientRect().height,
+        footerHeight= document.querySelector('footer').getBoundingClientRect().height;
+
+    if (window.location.href.includes('index.html')) {
+        document.querySelector('div.hero').style.height = String(window.innerHeight - navHeight - footerHeight) + 'px';
+    }
+
+    document.querySelector('div.content').style.height = String(window.innerHeight - navHeight - footerHeight) + 'px';
 };
 
 function addCurrent() {
     let current = document.querySelector('a');
 
-    if (current == window.location.href)
+    if (current == window.location.href) {
         current.classList.add('current');
+    }
 };
 
 
